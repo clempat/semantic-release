0a5e475269bb6d92f18add831df190ad387455b1
Clément Patout <clement.patout@quandoo.com>
2019-08-12 22:33:13 +0200

    fix: gitlab to github

--- docker.test.js
+++ docker.test.js
@@ -2,11 +2,11 @@ describe('docker config', () => {
   beforeAll(() => {
     process.env.GCR_JSON_KEY = new Buffer.from('test').toString('base64');
   });
-  it('should load the gitlab plugin', () => {
+  it('should load the github plugin', () => {
     const config = require('./docker');
     expect(config).toEqual(
       expect.objectContaining({
-        plugins: expect.arrayContaining(['@semantic-release/gitlab'])
+        plugins: expect.arrayContaining(['@semantic-release/github'])
       })
     );
   });
